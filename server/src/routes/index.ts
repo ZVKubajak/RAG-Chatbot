33ec2a156102ffdbc3183f72999a23b0d44a319f
VanceGoblin <zvkubajak@gmail.com>
2025-07-05 23:19:02 -0500

    Setup API Key Authenticate Middleware

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import apiRoutes from "./api/index";
 import chat from "../controllers/chat";
+import authenticate from "../middlewares/authenticate";
 
 const router = Router();
 
-router.use("/api", apiRoutes);
+router.use(
+  "/api",
+  // authenticate,
+  apiRoutes
+);
 router.post("/chat", chat);
 
 export default router;
